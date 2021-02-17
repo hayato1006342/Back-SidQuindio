6ba307ccccf7b1f29bfa1e87dc16431d63cf2ec5
Luis Eduardo Rodriguez <luiseduardor302gmail.com>
2021-02-17 15:44:20 -0500

    Cambios en rutas

--- Back/app.py
+++ Back/app.py
@@ -6,19 +6,17 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.add_url_rule(user["login_user"], view_func=user["login_user_controllers"])
-app.add_url_rule(user["registro_user"], view_func=user["registro_user_controllers"])
-app.add_url_rule(user["recuperacion_user"], view_func=user["recuperacion_user_controllers"])
-app.add_url_rule(user["detalles_lug"], view_func=user["detalles_user_controllers"])
-app.add_url_rule(user["historial_user"], view_func=user["historial_user_controllers"])
+app.add_url_rule(user["register_user"], view_func=user["registro_user_controllers"])
+app.add_url_rule(user["recovery_user"], view_func=user["recuperacion_user_controllers"])
+app.add_url_rule(user["details_lug"], view_func=user["detalles_user_controllers"])
+app.add_url_rule(user["history_user"], view_func=user["historial_user_controllers"])
 
 
-app.add_url_rule(admin["crear_admin"], view_func=admin["crear_admin_controllers"])
+app.add_url_rule(admin["create_admin"], view_func=admin["crear_admin_controllers"])
 app.add_url_rule(admin["panel_admin"], view_func=admin["panel_admin_controllers"])
 
 
-app.add_url_rule(lugares["atractivos_lug"], view_func=lugares["atract_lugares_controllers"])
-app.add_url_rule(lugares["hoteles_lug"], view_func=lugares["hoteles_lugares_controllers"])
-
-
+app.add_url_rule(places["sites_lug"], view_func=places["atract_lugares_controllers"])
+app.add_url_rule(places["hotels_lug"], view_func=places["hoteles_lugares_controllers"])
 
 
--- Back/routes.py
+++ Back/routes.py
@@ -2,19 +2,19 @@ from controllers import LoginUserControllers, RegistroUserControllers, Recuperac
 
 user = {
     "login_user": "/api/v01/login", "login_user_controllers": LoginUserControllers.as_view("login_api"),
-    "registro_user": "/api/v01/registro", "registro_user_controllers": RegistroUserControllers.as_view("register_api"),
-    "recuperacion_user": "/api/v01/recuperacion", "recuperacion_user_controllers": RecuperacionUserControllers.as_view("recuperacion_api"),
-    "detalles_lug":"/api/v01/detalles","detalles_user_controllers": DetallesUserControllers.as_view("detalles_api"),
-    "historial_user":"/api/v01/historial","historial_user_controllers": HistorialUserControllers.as_view("historial_api")
+    "register_user": "/api/v01/registro", "registro_user_controllers": RegistroUserControllers.as_view("register_api"),
+    "recovery_user": "/api/v01/recuperacion", "recuperacion_user_controllers": RecuperacionUserControllers.as_view("recodery_api"),
+    "details_lug":"/api/v01/detalles","detalles_user_controllers": DetallesUserControllers.as_view("details_api"),
+    "history_user":"/api/v01/historial","historial_user_controllers": HistorialUserControllers.as_view("history_api")
 }
 
 
 admin = {
-	"crear_admin":"/api/v01/crear","crear_admin_controllers": CrearAdminControllers.as_view("crear_api"),
+	"create_admin":"/api/v01/crear","crear_admin_controllers": CrearAdminControllers.as_view("create_api"),
 	"panel_admin":"/api/v01/panel","panel_admin_controllers": PanelAdminControllers.as_view("panel_api")
 }
 
-lugares = {
-	"atractivos_lug":"/api/v01/atractivos","atract_lugares_controllers": AtractivosLugaresControllers.as_view("Contenido_api"),
-	"hoteles_lug":"/api/v01/hoteles","hoteles_lugares_controllers": HotelesLugaresControllers.as_view("Hoteles_api")
+places = {
+	"sites_lug":"/api/v01/atractivos","atract_lugares_controllers": AtractivosLugaresControllers.as_view("Contenido_api"),
+	"hotels_lug":"/api/v01/hoteles","hoteles_lugares_controllers": HotelesLugaresControllers.as_view("Hotels_api")
 }
